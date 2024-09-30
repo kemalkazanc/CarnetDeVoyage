babe63471cdd9a6b5df7dab1f66901965fb8d7ef
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-09-30 14:47:24 +0200

    Initial Commit

---
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnetDeVoyageApp.swift
+//  CarnetDeVoyage
+//
+//  Created by Kemal Kazanc on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
